64bffadc78103be26fb9519c40f34546f83e6594
Renaider <Renaider12@gmail.com>
2021-09-16 18:07:46 +0500

    S2.13A

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,14 +6,14 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Катет 1:");
+            Console.WriteLine("Число 1:");
             string c = Console.ReadLine();
             int a = Convert.ToInt32(c);
-            Console.WriteLine("Катет 2:");
+            Console.WriteLine("Число 2:");
             string d = Console.ReadLine();
             int b = Convert.ToInt32(d);
-            double g = Math.Sqrt(a * a + b * b);
-            Console.WriteLine("Периметр прямоугольного треугольника: " + (Math.Sqrt(a * a + b * b) + a + b));
+            Console.WriteLine("Среднее арифметическое модулей: " + ((double)(Math.Abs(a)+Math.Abs(b))/2));
+            Console.WriteLine("Среднее геометрическое модулей: " + Math.Sqrt(Math.Abs(a)*Math.Abs(b)));
         }
     }
 }
