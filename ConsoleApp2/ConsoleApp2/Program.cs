1077710482e5f5a69fc7bb2ebfd19262d25cca10
Renaider <Renaider12@gmail.com>
2021-09-17 01:40:11 +0500

    S2.18

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,19 +6,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Координаты точки 1 по оси абсцисс:");
-            string f = Console.ReadLine();
-            int x1 = Convert.ToInt32(f);
-            Console.WriteLine("Координаты точки 1 по оси ординат:");
-            string d = Console.ReadLine();
-            int y1 = Convert.ToInt32(d);
-            Console.WriteLine("Координаты точки 2 по оси абсцисс:");
+            Console.WriteLine("Основание 1:");
             string c = Console.ReadLine();
-            int x2 = Convert.ToInt32(c);
-            Console.WriteLine("Координаты точки 2 по оси ординат:");
-            string g = Console.ReadLine();
-            int y2 = Convert.ToInt32(g);
-            Console.WriteLine("Расстояние между точками: " + Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
+            int a = Convert.ToInt32(c);
+            Console.WriteLine("Основание 2:");
+            string d = Console.ReadLine();
+            int b = Convert.ToInt32(d);
+            Console.WriteLine("Высота:");
+            string f = Console.ReadLine();
+            int h = Convert.ToInt32(f);
+            double side = Math.Sqrt(Math.Abs(a - b) * Math.Abs(a - b) + h * h);
+            Console.WriteLine("Периметр трапеции: " + (2 * side + a + b));
         }
     }
 }
