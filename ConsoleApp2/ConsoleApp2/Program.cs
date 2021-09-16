61cfdb4ee2dd9d4bab9c2f36fc4b9246a7f58a43
Renaider <Renaider12@gmail.com>
2021-09-16 18:28:07 +0500

    S2.15

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,14 +6,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Сторона 1:");
+            Console.WriteLine("Число 1:");
             string c = Console.ReadLine();
             int a = Convert.ToInt32(c);
-            Console.WriteLine("Сторона 2:");
+            Console.WriteLine("Число 2:");
             string d = Console.ReadLine();
             int b = Convert.ToInt32(d);
-            Console.WriteLine("Периметр прямоугольника: " + (2*a+2*b));
-            Console.WriteLine("Длина диагонали прямоугольника: " + Math.Sqrt(a*a+b*b));
+            Console.WriteLine("Сумма чисел: " + (a+b));
+            Console.WriteLine("Разность чисел: " + (a-b));
+            Console.WriteLine("Произведение чисел: " + (a * b));
+            Console.WriteLine("Частное от деления числа 1 на число 2: " + (double)a/b);
         }
     }
 }
