bfa8f0dc0198073890276cdb3d8d98f7625ce438
Renaider <Renaider12@gmail.com>
2021-09-16 17:41:10 +0500

    S2.10

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,13 +6,20 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Количество жителей:");
-            string a = Console.ReadLine();
-            int m = Convert.ToInt32(a);
-            Console.WriteLine("Площадь государства:");
-            string b = Console.ReadLine();
-            int s = Convert.ToInt32(b);
-            Console.WriteLine("Плотность населения: " + (double)m/s);
+            Console.WriteLine("Коэффициент a:");
+            string c = Console.ReadLine();
+            if (c != "0")
+            {
+                int a = Convert.ToInt32(c);
+                Console.WriteLine("Свободный член b:");
+                string d = Console.ReadLine();
+                int b = Convert.ToInt32(d);
+                Console.WriteLine("Решение уравнения ax+b=0: " + (double)-b / a);
+            }
+            else
+            {
+                Console.WriteLine("Коэффициент a не может быть равен 0, введите другое значение");
+            }
         }
     }
 }
