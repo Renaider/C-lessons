17f9303fa0d3f6db3fead0244f1dddb8f1407149
Renaider <Renaider12@gmail.com>
2021-09-17 01:27:58 +0500

    S2.16

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,16 +6,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Число 1:");
-            string c = Console.ReadLine();
-            int a = Convert.ToInt32(c);
-            Console.WriteLine("Число 2:");
+            Console.WriteLine("Сторона 1:");
+            string f = Console.ReadLine();
+            int a = Convert.ToInt32(f);
+            Console.WriteLine("Сторона 2:");
             string d = Console.ReadLine();
             int b = Convert.ToInt32(d);
-            Console.WriteLine("Сумма чисел: " + (a+b));
-            Console.WriteLine("Разность чисел: " + (a-b));
-            Console.WriteLine("Произведение чисел: " + (a * b));
-            Console.WriteLine("Частное от деления числа 1 на число 2: " + (double)a/b);
+            Console.WriteLine("Сторона 3:");
+            string g = Console.ReadLine();
+            int c = Convert.ToInt32(g);
+            Console.WriteLine("Объём параллелепипеда: " + (a*b*c));
+            Console.WriteLine("Площадь боковой поверхности: " + (2*a*c+2*b*c));
         }
     }
 }
