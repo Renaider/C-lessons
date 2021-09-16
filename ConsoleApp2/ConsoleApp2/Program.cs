a721524e8a488f7e2c97458f727f5b52fac233a8
Renaider <Renaider12@gmail.com>
2021-09-16 18:19:24 +0500

    S2.14

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -6,14 +6,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Число 1:");
+            Console.WriteLine("Основание 1:");
             string c = Console.ReadLine();
             int a = Convert.ToInt32(c);
-            Console.WriteLine("Число 2:");
+            Console.WriteLine("Основание 2:");
             string d = Console.ReadLine();
             int b = Convert.ToInt32(d);
-            Console.WriteLine("Среднее арифметическое модулей: " + ((double)(Math.Abs(a)+Math.Abs(b))/2));
-            Console.WriteLine("Среднее геометрическое модулей: " + Math.Sqrt(Math.Abs(a)*Math.Abs(b)));
+            Console.WriteLine("Высота:");
+            string f = Console.ReadLine();
+            int h = Convert.ToInt32(f);
+            double side = Math.Sqrt(Math.Abs(a - b) * Math.Abs(a - b) + h*h);
+            Console.WriteLine("Периметр трапеции: " + (2*side + a + b));
         }
     }
 }
